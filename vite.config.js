ba1d9d067aad25f928d657d22cc16ba7873a5f6e
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-03 10:52:05 -0500

    edit server

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Makes the app available on all network interfaces
+    port: 3000, // You can specify a different port if needed
+  },
+});
