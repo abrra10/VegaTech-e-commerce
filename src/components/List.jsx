08ccd79b6e03d809f29bd184696b1081904b66bd
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-04 18:04:18 -0500

    back to the first list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,29 +3,16 @@ import Card from "./Card";
 import useFetch from "../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
-  if (!catId) {
-    return <div>Error: Category ID is not defined.</div>;
-  }
-
-  const url =
-    `/products?populate=*&[filters][categories][id]=${catId}` +
-    (subCats.length
-      ? `${subCats
-          .map((item) => `&[filters][sub_categories][id][$eq]=${item}`)
-          .join("")}`
-      : "") +
-    `&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`;
-
-  console.log("Fetching URL:", url); // Debugging line
-
-  const { data, loading, error } = useFetch(url);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4">
       {loading
         ? "Loading..."
-        : error
-        ? "Error loading products."
         : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
   );
