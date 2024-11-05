a6007e05c09d94d5ea7ebf49bc8fe2960b9c2e15
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-05 10:36:50 -0500

    list updated

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,11 +3,33 @@ import Card from "./Card";
 import useFetch from "../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
-  const { data, loading, error } = useFetch(
-    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
-      (item) => `&[filters][sub_categories][id][$eq]=${item}`
-    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
-  );
+  // Build query string dynamically
+  const buildQuery = () => {
+    const filters = [`populate=*`];
+
+    if (catId) {
+      filters.push(`[filters][categories][id]=${catId}`);
+    }
+
+    if (subCats && subCats.length > 0) {
+      subCats.forEach((item) => {
+        filters.push(`[filters][sub_categories][id][$eq]=${item}`);
+      });
+    }
+
+    if (maxPrice) {
+      filters.push(`[filters][price][$lte]=${maxPrice}`);
+    }
+
+    if (sort) {
+      filters.push(`sort=price:${sort}`);
+    }
+
+    // Join all filters with '&' to form the final query string
+    return `/products?${filters.join("&")}`;
+  };
+
+  const { data, loading, error } = useFetch(buildQuery());
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4">
