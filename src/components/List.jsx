c8e71d1e16bd5950b19afe0e0f9d40ee72499418
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-06 16:34:09 -0500

    list 3 time changed

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "./Card";
 import useFetch from "../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, slug }) => {
-  const { data, loading, error } = useFetch(
-    `/products?populate=*&[filters][categories][slug][$eq]=${slug}${subCats.map(
-      (item) => `&[filters][sub_categories][id][$eq]=${item}`
-    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
-  );
+  // Construct the query URL using the category slug
+  const query = `/products?populate=*&[filters][categories][slug][$eq]=${slug}${subCats.map(
+    (item) => `&[filters][sub_categories][id][$eq]=${item}`
+  )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`;
+
+  useEffect(() => {
+    console.log("Fetch URL:", query); // This logs the query URL for debugging
+  }, [query]);
+
+  const { data, loading, error } = useFetch(query);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4">
-      {loading
-        ? "Loading..."
-        : data?.map((item) => <Card item={item} key={item.id} />)}
+      {loading && "Loading..."}
+      {error && <p>Error loading products: {error.message}</p>}
+      {data?.map((item) => (
+        <Card item={item} key={item.id} />
+      ))}
     </div>
   );
 };
