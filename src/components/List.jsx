9290da10c4a6ce46502415114b6fb393c4d0e401
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-06 16:07:27 -0500

    list changed again 1

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,16 +3,28 @@ import Card from "./Card";
 import useFetch from "../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
-  const { data, loading, error } = useFetch(
-    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
-      (item) => `&[filters][sub_categories][id][$eq]=${item}`
-    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
-  );
+  // Construct the filter for sub-categories
+  const subCategoryFilter = subCats.length
+    ? subCats
+        .map((item) => `&[filters][sub_categories][id][$eq]=${item}`)
+        .join("")
+    : "";
+
+  // Construct the URL
+  const url = `/products?populate=*&[filters][categories][id]=${catId}${subCategoryFilter}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`;
+
+  // Fetch data
+  const { data, loading, error } = useFetch(url);
+
+  // Log URL for debugging
+  console.log("Fetching URL:", url);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-4">
       {loading
         ? "Loading..."
+        : error
+        ? "Error loading data"
         : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
   );
