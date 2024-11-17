c0ba020b7ef56f1980c906d9eb9a97ebff5880b6
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-17 17:58:36 -0500

    kmlna

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -14,11 +14,11 @@ const ThankYouPage = () => {
       <h1 className="text-3xl font-bold font-Secondary text-center text-gray-800 mb-2">
         ! شكرا على ثقتكم
       </h1>
-      <p className="text-center text-gray-600 font-Secondary text-lg max-w-lg">
+      <p className="text-center text-gray-600 font-Secondary font-bold text-lg max-w-lg">
         Un de nos employés examinera votre commande et vous contactera bientôt.
       </p>
-      <p className="text-center text-gray-600 font-Secondary text-lg max-w-lg">
-        سيتم مراجعة الطلب من قبل موظفينا والاتصال بكم قريبًا.
+      <p className="text-center text-gray-600 font-Secondary font-bold text-lg max-w-lg">
+        سيتم مراجعة الطلب من قبل موظفينا والاتصال بكم قريبًا
       </p>
     </div>
   );
