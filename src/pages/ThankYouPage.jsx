eab68c860127aeb9a4cd555e79239ea6dcbbdc03
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-17 17:52:26 -0500

    krahna

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -3,7 +3,7 @@ import { HiCheckCircle } from "react-icons/hi2";
 
 const ThankYouPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 pt-40">
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 pt-24">
       {/* Verification Icon */}
       <HiCheckCircle className="text-green-500 text-6xl mb-4" />
 
