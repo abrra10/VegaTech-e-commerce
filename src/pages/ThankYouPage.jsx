93d118e390386649c25646c1da5abbef359c2427
Benacer Taha Walid <88546423+abrra10@users.noreply.github.com>
2024-11-17 17:45:59 -0500

    thnk you page

--- src/pages/ThankYouPage.jsx
+++ src/pages/ThankYouPage.jsx
@@ -3,16 +3,21 @@ import { HiCheckCircle } from "react-icons/hi2";
 
 const ThankYouPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center h-4/6 bg-gray-100">
       {/* Verification Icon */}
       <HiCheckCircle className="text-green-500 text-6xl mb-4" />
 
       {/* Thank You Message */}
       <h1 className="text-3xl font-bold font-Secondary text-center text-gray-800 mb-2">
-        Merci pour votre confiance ! شكرا على ثقتكم
+        Merci pour votre confiance !
+      </h1>
+      <h1 className="text-3xl font-bold font-Secondary text-center text-gray-800 mb-2">
+        ! شكرا على ثقتكم
       </h1>
       <p className="text-center text-gray-600 font-Secondary text-lg max-w-lg">
         Un de nos employés examinera votre commande et vous contactera bientôt.
+      </p>
+      <p className="text-center text-gray-600 font-Secondary text-lg max-w-lg">
         سيتم مراجعة الطلب من قبل موظفينا والاتصال بكم قريبًا.
       </p>
     </div>
